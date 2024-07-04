f85abc6407ab47a94307d4c97e5723a68e85985b
Deepak Sharma <dsharma2828@gmail.com>
2024-07-04 16:05:13 +0530

    Model for user is created

---
+++ src/main/java/com/deepaksharma/digital_library/model/User.java
@@ -0,0 +1,47 @@
+package com.deepaksharma.digital_library.model;
+
+import com.deepaksharma.digital_library.enums.UserStatus;
+import com.deepaksharma.digital_library.enums.UserType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(length = 30)
+    String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    String email;
+
+    @Column(unique = true, length = 12)
+    String phoneNo;
+
+    String address;
+
+    @Enumerated(value = EnumType.STRING)
+    UserType userType;
+
+    @Enumerated(value = EnumType.STRING)
+    UserStatus userStatus;
+
+    @CreationTimestamp
+    Date createdOn;
+
+    @UpdateTimestamp
+    Date updatedOn;
+}
