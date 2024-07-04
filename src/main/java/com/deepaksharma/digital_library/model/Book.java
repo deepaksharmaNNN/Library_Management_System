a5a0245b5b883899fb933b2a6a55d5391b9657aa
Deepak Sharma <dsharma2828@gmail.com>
2024-07-04 16:14:55 +0530

    Model for book is added

--- src/main/java/com/deepaksharma/digital_library/model/Book.java
+++ src/main/java/com/deepaksharma/digital_library/model/Book.java
@@ -1,2 +1,40 @@
-package com.deepaksharma.digital_library.model;public class Book {
+package com.deepaksharma.digital_library.model;
+
+import com.deepaksharma.digital_library.enums.BookType;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(nullable = false, length = 50)
+    String bookTitle;
+
+    @Column(nullable = false, length = 10, unique = true)
+    String bookNo;
+
+    int securityDeposit;
+
+    @Enumerated(value = EnumType.STRING)
+    BookType bookType;
+
+    @CreationTimestamp
+    Date createdOn;
+
+    @UpdateTimestamp
+    Date updatedOn;
 }
